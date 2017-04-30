fc39cd88dd0fb7da569831905cb5969bc30ab8d7
Shivam Kurtarkar <Shivam Kurtarkar>
2017-04-30 10:25:29 +0530

    ui

--- sad_v4/attendence_page.php
+++ sad_v4/attendence_page.php
@@ -8,10 +8,42 @@
   		echo "Failed to connect to MySQL: " . mysqli_connect_error();
   		die();
   	}
+  	 $query ="SELECT `first_name`,`last_name` FROM `teacher` WHERE id='$teacher_id';";
+  	$result = mysqli_query($conn, $query);
+  	$teacher_name=mysqli_fetch_row($result);
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
+		<title>AMS</title>
+	      <meta charset="UTF-8">
+	      <meta name="viewport" content="width=device-width, initial-scale=1">
+	      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	      <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
+	      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+	      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	      <style>
+	        html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
+	      </style>
+			<style>
+				footer {
+			        height: 50px;
+					width: 100%;
+					left: 0;
+			        right: 0;
+					bottom: 0;
+					text-align: center;
+				    position: relative;
+				}
+			    #main-wrapper{
+			    	top:100px;
+			        min-height: 100%;
+				    padding: 0 0 100px;
+				    position: relative;
+			    }
+
+			</style>
+
 		<script src="http://localhost/jquery/jquery-3.2.1.min.js"></script>
 		<style type="text/css" src="style.css"></style>
 		<script type="text/javascript" id="responsive" >
@@ -20,7 +52,7 @@
 					var $id=div.find("#roll").html();
 					var $first_name= div.find("#first_name").html();
 					var $last_name= div.find("#last_name").html();
-					var $attendence= div.find("#attendence").is(':checked')?"Present":"Absent";
+					var $attendence= div.find("#present_btn").html();
 					var $periodcode=$("#periodcodeselect").val();
 					var $date =$("#date").val() ;
 					$.post("save_attendence.php",
@@ -48,6 +80,12 @@
 		</script>
 		<script type="text/javascript">
 			function loadlist(){
+					resp = function (div){ ;};
+					t=false;
+
+					refresh_contents();
+				};
+				function refresh_contents(){
 					$periodcode=$("#periodcodeselect").val();
 					$date =$("#date").val() ;
 					alert($periodcode+" "+$date);
@@ -64,11 +102,54 @@
 				            alert("Data: " + data + "\nStatus: " + status);
 				        }
 					);
+					return 1;
 				};
+				/*
+				function check_checkbox(div)
+				{
+					var list =(div.getElementsByTagName('span'));
+					for (var i=0;i<list.length;i++){
+						alert(list[i].getAttribute('id')+list[i].innerHTML)
+						if(list[i].getAttribute('id')=="present_btn")
+							{
+								if(list[i].innerHTML=="Present")
+								{
+									list[i].innerHTML="Absent";
+									list[i].className="w3-button w3-white w3-xlarge w3-right w3-hover-red";
+								}
+								else
+								{
+									list[i].innerHTML="Present";
+									list[i].className="w3-button w3-white w3-xlarge w3-right w3-hover-green";
+								}
+							}
+				    }
+				}*/
+				function check_checkbox($div)
+				{
+					var list =($div.find('span'));
+					for (var i=0;i<list.length;i++){
+						alert($(list[i]).attr('id')+$(list[i]).html())
+						if($(list[i]).attr('id')=="present_btn")
+							{
+								if($(list[i]).html()=="Present")
+								{
+									$(list[i]).html("Absent");
+									$(list[i]).attr('class',"w3-button w3-white w3-xlarge w3-right w3-hover-red");
+								}
+								else
+								{
+									$(list[i]).html("Present");
+									$(list[i]).attr('class',"w3-button w3-white w3-xlarge w3-right w3-hover-green");
+								}
+							}
+				    }
+				}
 		</script>
 		<script type="text/javascript">
+			var t=false;
+
 			$(document).ready(function(){
-				var t=false;
 				$(document).on("click","#SaveForm",function(){
 					if(t==false)
 					{
@@ -85,10 +166,16 @@
 						}
 					else
 						alert("valid date not set");
+		
+						alert($("#SaveForm").attr('class','w3-btn  btn w3-hover-green w3-text-green'));
+					
 				});
-				$(document).on("click",".attcheck",function(){
-					var $div=$(this).closest("div");
-					resp($div);
+				$(document).on("click","#present_btn",function(){
+					var $div=$(this).closest("li");
+					check_checkbox($div);
+					resp($(this).closest("li"));
+
+					
 				});
 			});
 		</script>
@@ -127,18 +214,18 @@
 						$(b).find("#first_name").text()+$(b).find("#last_name").text();
 				},
 				by_check:function(a,b){
-					return ($(a).find("#attendence").is(":checked")) >
-						($(b).find("#attendence").is(":checked"));
+					return ($(a).find("#present_btn").text()) >
+						($(b).find("#present_btn").text());
 				}
 			};
 			function sorter(comp_by){
 				var sort=[].sort();
-				var $StudentListContainer =$("div.student");
+				var $StudentListContainer =$("li.student");
 				var OrderedDivs =$StudentListContainer.sort(comp_by);
 				$("#StudentListContainer").html(OrderedDivs);
 			};
 			function rev_sorter(comp_by){
-				var $StudentListContainer =$("div.student");
+				var $StudentListContainer =$("li.student");
 				var OrderedDivs =$StudentListContainer.sort(function(a,b){
 					return !comp_by(a,b)
 				});
@@ -161,24 +248,40 @@
 				}
 			};
 		</script>
-		<style >
-			#first_name: {
-				float: left;
-				width: 80px;
-			}
-			#last_name: {
-				float: left;
-				width: 80px;
-			}
-		</style>
 	</head>
 
 	<body>
-		<h2>Attendence Management System</h2><br/>
-		<h3>Class :
-		<?php echo $course_id ." ".$course_name ." ". $course_year ." sem: ".$sem;	?>	
-		</h3>
-		<a>Date:</a> <input type="date" id= date name="date" onchange="myfunction()"><br/><br/>
+		<!--Nav bar-->
+		    <div class="w3-top w3-card-4">
+		       <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
+		        <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+		        <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Assignment Management System</a>
+		        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account">
+		        <?php echo $teacher_name[0]." ".$teacher_name[1];?>
+		        &nbsp&nbsp<img src="http://localhost/w3/w3images/avatar3.png" class="w3-circle" style="height:25px;width:25px" alt="Avatar"></a>
+		       </div>
+		    </div>
+	    <!--Nav bar end-->
+	    	    <!--main page-->
+	    <div class="w3-container  " style="max-width:1400px;margin-top:80px; ">    
+			<div class="w3-panel  w3-theme-d2 w3-card-4">
+				<h3>Class :
+				<?php echo $course_id ." ".$course_name ." ". $course_year ." sem: ".$sem;	?>	
+				</h3>
+				<br/>
+				<a>Date:</a> <input type="date" class="w3-theme-d2" id= date name="date" onchange="myfunction()">
+				<a id="day"></a>
+
+				<select id="periodcodeselect" class="w3-select w3-theme-d2" onchange="loadlist()">
+					<option value="m9">Morning 9 O'clock </option>
+					<option value="m10">Morning 10 O'clock </option>
+					<option value="m11">Morning 11 O'clock </option>
+					<option value="m12">Morning 12 O'clock </option>
+					<option value="e2">Noon 2 O'clock </option>
+					<option value="e3">Evening 3 O'clock </option>
+					<option value="e4">Evening 4 O'clock </option>
+				</select><br/><br/>
+			</div>
 		<!--<script>
 		    $(function(){           
 		        if (!Modernizr.inputtypes.date) {
@@ -189,27 +292,32 @@
 		        }
 		    });
 		</script>-->
-		<a id="day"></a>
+			
 
-		<select id="periodcodeselect">
-			<option value="m9">Morning 9 O'clock</option>
-			<option value="m10">Morning 10 O'clock</option>
-			<option value="m11">Morning 11 O'clock</option>
-			<option value="m12">Morning 12 O'clock</option>
-			<option value="e2">Noon 2 O'clock</option>
-			<option value="e3">Evening 3 O'clock</option>
-			<option value="e4">Evening 4 O'clock</option>
-		</select><br/><br/>
-		<input type="button" id="byid" name="byid" value="byid" onclick="reorderdiv('by_id')">
-		<input type="button" id="byper" name="byper" value="byper" onclick="reorderdiv('by_per')">
-		<input type="button" id="byalph" name="byalph" value="byalph" onclick="reorderdiv('by_name')">
-		<input type="button" id="bycheck" name="bycheck" value="bycheck" onclick="reorderdiv('by_check')">
-		<br/>
-		<a>Student List</a>
-		<div  id="StudentList">			
+
+			<div class="w3-card-4 w3-padding w3-border">
+				<a>Student List</a>
+				
+				<ul class="w3-ul ">
+				<li class='w3-padding-16'>
+					<span class="w3-button w3-bar-item" id="byid" onclick="reorderdiv('by_id')"> Id</span>
+					<span class="w3-button w3-bar-item" id="byalph"  onclick="reorderdiv('by_name')" >Name</span>
+					<span class="w3-button w3-bar-item" id="bycheck"  onclick="reorderdiv('by_check')" >Check</span>
+					<span class="w3-button w3-bar-item" id="byper"  onclick="reorderdiv('by_per')" >Percent</span>
+				</li>
+				<li></li>
+				</ul>
+
+				<div  id="StudentList">			
+				</div>
+			</div>
+
 		</div>
-
 		<br/>
-		<br/> <h4>made by shivam</h4>
+		<footer class="w3-container w3-theme-d5 ">
+			<p>
+				<a>by shivam</a>
+			</p>
+		</footer>
 	</body>
 </html>
--- sad_v4/thome.php
+++ sad_v4/thome.php
@@ -1,11 +1,3 @@
-<!DOCTYPE HTML>
-<html lang="en">
-	<head>
-	</head>
-
-	<body>
-		<h2>Attendence Management System</h2><br/>
-		<h3>TEACHER : 
 <?php
 	$teacher_id=$_GET['usr_id'];
 
@@ -14,33 +6,49 @@
 	if (mysqli_connect_errno())
   	{
   		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  		die();
   	}
   	$query ="SELECT `first_name`,`last_name` FROM `teacher` WHERE id='$teacher_id';";
   	$result = mysqli_query($conn, $query);
   	$teacher_name=mysqli_fetch_row($result);
-	printf( "%s %s", $teacher_name[0], $teacher_name[1]);
+		function get_gender($roll){
+			$str="SELECT gender  FROM `teacher` WHERE 
+			id=".$GLOBALS['teacher_id'].";";
+			$val= mysqli_fetch_row(mysqli_query($GLOBALS['conn'], $str))[0];
+			return $val;
+		}
 ?>		
-		</h3>
-		<a>Courses</a>
-		<div id="courselist">
-<?php
-	
-		
-	$query ="SELECT * FROM `course` WHERE teacher_id='$teacher_id';";
-	$result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_all($result);
-    printf("<br/>");
-    foreach ($row as  $value) {
-    	# code...
-    	?>
-    	<div  onclick='fun(this)' > 
-    		&nbsp &nbsp <span id='course_id'><?php echo$value[0]?> </span>
-    		&nbsp <span id='course_name'><?php echo$value[1]?></span> 
-    		&nbsp <span id='course_year'><?php echo$value[3]?></span>
-    		&nbsp sem:&nbsp<span id='sem'><?php echo$value[4]?></span>
-    	</div><br/>
-		
-    	<script type='text/javascript'>
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>AMS</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
+      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <style>
+        html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
+      </style>
+		<style>
+			footer {
+		        height: 50px;
+				width: 100%;
+				left: 0;
+		        right: 0;
+				bottom: 0;
+				text-align: center;
+			    position: absolute;
+			}
+		    #main-wrapper{
+		    	top:100px;
+		        min-height: 100%;
+			    padding: 0 0 100px;
+			    position: relative;
+		    }
+		</style>
+				<script type='text/javascript'>
 			function fun(div){
 				var atributes=[];
 			    atributes.push("attendence_page.php?teacher_id="+
@@ -51,34 +59,65 @@
 			    }
 			    var link=atributes.join('&');
 			    alert(link);
-			     //var createA = document.createElement('a');
-			     //   createA.setAttribute('href', link);
-			     //   createA.click();
 			    window.location=link;
 			}
-			</script>
-			<?php
-    }
-    mysqli_free_result($result);
-    mysqli_close($conn);
+		</script>
+	</head>
 
-	#include("thome_bottom.php");
-?>
-
+	<body>
+		<!--Nav bar-->
+	    <div class="w3-top w3-card-4">
+	       <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
+	        <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+	        <a href="#" class="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i class="fa fa-home w3-margin-right"></i>Assignment Management System</a>
+	        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account">
+	        <?php echo $teacher_name[0]." ".$teacher_name[1];?>
+	        &nbsp&nbsp<img  src="http://localhost/w3/w3images/avatar<?php echo get_gender($teacher_id)=="Male"? 3:4 ?>.png" class="w3-circle" style="height:25px;width:25px" alt="Avatar"></a>
+	       </div>
+	    </div>
+	    <!--Nav bar end-->
+	    <!--main page-->
+	    <div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
+      <!-- The Grid -->
+      		 <div class="w3-card-4 w3-padding w3-border">
+				<h3>Courses</h3>
+			</div>
+		<ul id="courselist" class="w3-ul">
+<?php
+	
+		
+	$query ="SELECT * FROM `course` WHERE teacher_id='$teacher_id';";
+	$result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_all($result);
+    printf("<br/>");
+    foreach ($row as  $value) {
+    	# code...
+    	?>
+    	<li  class='student w3-padding-16' onclick='fun(this)' > 
+    		&nbsp &nbsp <span id='course_id'><?php echo$value[0]?> </span>
+    		&nbsp <span id='course_name'><?php echo$value[1]?></span> 
+    		&nbsp <span id='course_year'><?php echo$value[3]?></span>
+    		&nbsp sem:&nbsp<span id='sem'><?php echo$value[4]?></span>
+    		<br/>
+    	</li>
+		
+    	
+	<?php
+    } ?>
+    <li></li>
+		</ul>
 		</div>
-		<!--<script type="text/php" src="courselist.php"> </script>-->
-		<br/>
-		<br/>
-		<!--<p id="demo"></p>-->
-		<br/>
-		<br/>
-
-		<br/> <h4>made by shivam</h4>
+		<footer class="w3-container w3-theme-d5 w3-bottom">
+			<p>
+				<a>by shivam</a>
+			</p>
+		</footer>
 	</body>
 </html>
-
-
-
+<?php 
+    mysqli_free_result($result);
+    mysqli_close($conn);
+?>	
 
 
 
